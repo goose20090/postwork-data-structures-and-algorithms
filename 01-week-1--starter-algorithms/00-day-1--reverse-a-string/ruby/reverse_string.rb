1d37ca461a9e7b30b23456dedf3cb9a26b45717d
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-08-15 10:54:29 +0100

    reverse string problem done

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
@@ -1,12 +1,41 @@
 def reverse_string(str)
-  # type your code in here
+
+  # Split the str into an array of characters
+
+  strArr = str.split('')
+
+  # Initialise an array to hold the reversed characters
+
+  rvStrArr = []
+
+  # Iterate through split string array
+
+  strArr.each do |char|
+
+    # Add each character to front of other array (designed to hold reversed characters)
+
+    rvStrArr.unshift(char)
+
+  end
+
+  
+  # join array with reversed characters into new string
+
+  rvStr = rvStrArr.join('')
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
   puts "=>", reverse_string('hi')
 
-  puts
+  puts "Expecing: ''"
+  puts "=>", reverse_string('')
+
+  puts "Expecing: 'a'"
+  puts "=>", reverse_string('a')
+
+  puts "Expecing: 'lionoil'"
+  puts "=>", reverse_string('lionoil')
 
   puts "Expecting: 'ybabtac'"
   puts "=>", reverse_string('catbaby')
